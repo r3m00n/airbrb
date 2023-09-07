7c0eeebe550552f6ec98c6a75612d65fd6536a36
Merlin Förster <moin@merlin.hamburg>
2023-09-07 11:01:26 +0200

    🤏 compressed files and classnames

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,13 +18,16 @@ const Button = (props: ButtonProps) => {
       onClick={onClick}
       disabled={disabled}
       className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full
-      ${outline ? 'bg-white' : 'bg-rose-500'}
-      ${outline ? 'border-black' : 'border-rose-500'}
-      ${outline ? 'text-black' : 'text-white'}
-      ${small ? 'py-1' : 'py-3'}
-      ${small ? 'text-sm' : 'text-base'}
-      ${small ? 'font-light' : 'font-semibold'}
-      ${small ? 'border-[1px]' : 'border-2'}`}
+      ${
+        outline
+          ? 'bg-white border-black text-black'
+          : 'bg-rose-500 border-rose-500 text-white'
+      }
+      ${
+        small
+          ? 'py-1 text-sm font-light border-[1px]'
+          : 'py-3 text-base font-semibold border-2'
+      }`}
     >
       {Icon && <Icon className="absolute top-3 left-4" size={24} />}
       {label}
