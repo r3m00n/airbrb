5ae2ed3e6bdf29eb4f280cf001a027981d8496cc
Merlin Förster <moin@merlin.hamburg>
2023-09-08 18:14:42 +0200

    🔎 adding search and filter functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ToasterProvider from '@/providers/ToasterProvider';
 import Navbar from '@/components/navbar/Navbar';
 import RegisterModal from '@/components/modals/RegisterModal';
 import LoginModal from '@/components/modals/LoginModal';
+import SearchModal from '@/components/modals/SearchModal';
 import RentModal from '@/components/modals/RentModal';
 import getCurrentUser from '@/actions/getCurrentUser';
 
@@ -17,12 +18,14 @@ const font = Nunito({ subsets: ['latin'] });
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <RegisterModal />
         <LoginModal />
+        <SearchModal />
         <RentModal />
         <Navbar currentUser={currentUser} />
         <div className="pb-20 pt-28">{children}</div>
