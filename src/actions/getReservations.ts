64b9c116412467b2547931c1bccd99767fc0ba5f
Merlin Förster <moin@merlin.hamburg>
2023-09-08 15:42:55 +0200

    🪳 fixed bug and improved ux

--- src/actions/getReservations.ts
+++ src/actions/getReservations.ts
@@ -8,20 +8,24 @@ interface IParams {
 
 export default async function getReservations(params: IParams) {
   try {
-    const { authorId, listingId, userId } = params;
+    const { listingId, userId, authorId } = params;
 
     const query: any = {};
 
-    if (userId) query.userId = userId;
     if (listingId) query.listingId = listingId;
-    if (authorId) query.authorId = { userId: authorId };
+
+    if (userId) query.userId = userId;
+
+    if (authorId) query.listing = { userId: authorId };
 
     const reservations = await prisma.reservation.findMany({
       where: query,
       include: {
         listing: true,
       },
-      orderBy: { createdAt: 'desc' },
+      orderBy: {
+        createdAt: 'desc',
+      },
     });
 
     const safeReservations = reservations.map(reservation => ({
@@ -31,7 +35,7 @@ export default async function getReservations(params: IParams) {
       endDate: reservation.endDate.toISOString(),
       listing: {
         ...reservation.listing,
-        createdAt: reservation.createdAt.toISOString(),
+        createdAt: reservation.listing.createdAt.toISOString(),
       },
     }));
 
