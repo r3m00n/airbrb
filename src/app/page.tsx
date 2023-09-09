bc05c36a2cfe04a66082ae1c3a23d3812c9891c2
Merlin Förster <moin@merlin.hamburg>
2023-09-10 00:06:11 +0200

    💩 dirty fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,14 @@ import ListingCard from '@/components/listings/ListingCard';
 interface HomeProps {
   searchParams: IListingsParams;
 }
+
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
-  if (listings.length === 0) return <EmptyState showReset />;
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
 
   return (
     <Container>
@@ -23,5 +26,7 @@ const Home = async ({ searchParams }: HomeProps) => {
     </Container>
   );
 };
+
+export const dynamic = 'force-dynamic';
 
 export default Home;
